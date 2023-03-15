c96f3b8d0555c47e9ffdffa62d1731842e6b6f76
cristian <cristiandavidv@hotmail.com>
2023-03-15 10:28:06 -0500

    Added Policites

---
+++ task-managments-main/app/policies/enterprise_policy.rb
@@ -0,0 +1,25 @@
+class EnterprisePolicy < ApplicationPolicy
+  def index?
+    permissions.include? "view_enterprise"
+  end
+
+  def show?
+    permissions.include? "view_enterprise"
+  end
+
+  def create?
+    permissions.include? "create_enterprise"
+  end
+
+  def update?
+    permissions.include? "update_enterprise"
+  end
+
+  def update_enabled?
+    permissions.include? "update_enabled_enterprise"
+  end
+
+  def destroy?
+    permissions.include? "destroy_enterprise"
+  end
+end
